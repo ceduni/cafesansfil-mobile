ecac4a502d52e9e337f2ff63479b83a6172d5109
Grace <grace.lee@umontreal.ca>
2025-03-21 12:46:10 -0400

    update product_tab

--- app/lib/screens/article/products_tab.dart
+++ app/lib/screens/article/products_tab.dart
@@ -1,6 +1,10 @@
-// products_tab.dart
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:provider/provider.dart';
 import 'package:app/models/Cafe.dart';
+import 'package:app/provider/cafe_provider.dart';
+import 'package:app/services/StockService.dart';
+import 'package:app/screens/article/editMenuItem.dart';
 
 class ProductsTab extends StatelessWidget {
   final List<MenuItem> menuItems;
@@ -9,14 +13,152 @@ class ProductsTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return ItemMenuList(menuItems);
+  }
+
+  Widget ItemMenuList(List<MenuItem> menuItems) {
     return ListView.builder(
       itemCount: menuItems.length,
       itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(menuItems[index].name),
-          // Add more details or actions if needed.
+        MenuItem menuItem = menuItems[index];
+        return Slidable(
+          key: ValueKey(menuItem.itemId),
+          endActionPane: ActionPane(
+            extentRatio: 0.25,
+            motion: const DrawerMotion(),
+            children: [
+              SlidableAction(
+                onPressed: (context) {
+                  _showDeleteConfirmationDialog(context, menuItem);
+                },
+                backgroundColor: Colors.red,
+                foregroundColor: Colors.white,
+                icon: Icons.delete,
+                label: 'Delete',
+              ),
+            ],
+          ),
+          child: ListTile(
+            leading: menuItem.imageUrl.isNotEmpty
+                ? Image.network(
+                    menuItem.imageUrl,
+                    width: 50,
+                    height: 50,
+                    fit: BoxFit.cover,
+                    loadingBuilder: (BuildContext context, Widget child,
+                        ImageChunkEvent? loadingProgress) {
+                      if (loadingProgress == null) {
+                        return child;
+                      } else {
+                        return SizedBox(
+                          width: 50,
+                          height: 50,
+                          child: Center(
+                            child: CircularProgressIndicator(
+                              value: loadingProgress.expectedTotalBytes != null
+                                  ? loadingProgress.cumulativeBytesLoaded /
+                                      (loadingProgress.expectedTotalBytes ?? 1)
+                                  : null,
+                            ),
+                          ),
+                        );
+                      }
+                    },
+                    errorBuilder: (BuildContext context, Object error,
+                        StackTrace? stackTrace) {
+                      return const Icon(Icons.broken_image);
+                    },
+                  )
+                : const Icon(Icons.image_not_supported),
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Text(
+                    menuItem.name,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 16),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                Text(
+                  '\$${menuItem.price.toStringAsFixed(2)}',
+                  style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            subtitle: Text(
+              menuItem.description,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(color: Colors.grey),
+            ),
+            onTap: () {
+              _navigateToEditMenuItem(context, menuItem);
+            },
+          ),
         );
       },
+    );
+  }
+
+  void _showDeleteConfirmationDialog(BuildContext context, MenuItem item) {
+    String itemName = item.name;
+    String cafeSlug =
+        Provider.of<CafeProvider>(context, listen: false).selectedCafe!.slug;
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirm Deletion'),
+          content: Text('Are you sure you want to delete $itemName?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Dismiss the dialog
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () async {
+                try {
+                  final stockService = StockService();
+                  await stockService.removeMenuItem(cafeSlug, item.slug);
+
+                  Provider.of<CafeProvider>(context, listen: false)
+                      .fetchCafe(cafeSlug);
+                  
+                  // Show success message
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('$itemName deleted successfully')),
+                  );
+                  Navigator.of(context).pop(); // Dismiss the dialog
+                } catch (e) {
+                  // Handle error
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Failed to delete $itemName')),
+                  );
+                  Navigator.of(context).pop();
+                }
+              },
+              child: const Text('Delete'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _navigateToEditMenuItem(BuildContext context, MenuItem menuItem) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => EditMenuItemScreen(menuItem: menuItem),
+      ),
     );
   }
 }
